6fddf9831cfb09605ca177c0d12d8e17c04d0ce8
SaiSree941 <151171176+SaiSree941@users.noreply.github.com>
2025-06-09 15:23:16 +0530

    Delete controllers/user.controller.js

--- controllers/user.controller.js
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-console.log("hello");
-=======
-console.log("Sai Sree2")
->>>>>>> 1857f8d453a734ca60bc5764252dfef891ffdd87
-=======
-console.log("Sai Sree3")
->>>>>>> 6e2117768b0b4b85aff809022ff7a307a0a8ed94
